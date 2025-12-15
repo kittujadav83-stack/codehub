adcb903727a8c12e2ec965586e70922a3f8bebee
kittujadav83@gmail.com <Kittu@1379>
2025-12-15 19:57:20 +0530

    adding in new branch

--- pset_8_03_multiplication_table.py
+++ pset_8_03_multiplication_table.py
@@ -1,4 +1,4 @@
-2num = int(input("Enter the number : = " ))
+num = int(input("Enter the number : = " ))
 print(f"Multiplication table of {num} is :")
 for i in range(1 , 11):
 # print(str(num) + "x" + str(i) + "=" + str(num*i))
